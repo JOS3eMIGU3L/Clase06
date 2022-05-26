911a29efb7a392ec0dd9fb56e5500094523d1476
JOS3eMIGU3L <huamanpariapazajosemiguel@gmail.com>
2022-05-26 14:56:52 +0000

    .leer teclado

---
+++ subprogramas/ClaseGeneral.java
@@ -0,0 +1,30 @@
+
+package subprogramas;
+
+import java.util.Scanner;
+
+public class ClaseGeneral {
+
+
+        static Scanner leerT=new Scanner(System.in);
+        static Aritmetica obj=new Aritmetica();
+        static Persona objP=new Persona();
+
+
+    public static void main(String[] args) {
+        /*System.out.println("Ingrese el Primer valor");
+        int aX=leerT.nextInt();
+        System.out.println("Ingrese el Segundo Valor:");
+        int bX=leerT.nextInt();
+        int result=obj.sumar(aX, bX);
+        System.out.println("La Suma es:"+result);*/
+        System.out.println("Ingrese su Nombre:");
+        objP.nombre=leerT.next();
+    System.out.println("Ingrese su de DNI:");
+        objP.dni=leerT.next(); 
+        System.out.println("Ingrese una comida");
+        
+        objP.comer(leerT.next());;
+    }
+
+}
---
+++ subprogramas/LeerTeclado.java
@@ -0,0 +1,37 @@
+package subprogramas;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+
+/**
+ * LeerTeclado
+ */
+public class LeerTeclado {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+    
+    public int print(String mensaje, int valor) {
+        return 0;
+        }
+    
+        public String print(String mensaje, String valor) {
+         return "0";
+    
+    }   
+        public float print(String mensaje, float valor) {
+        return 0;
+    
+    
+     }  public double print(String mensaje, double valor) {
+        return 0;
+    
+    
+     }  public boolean print(String mensaje, boolean valor) {
+        return true;
+     }
+ 
+
+}
+    
+    
